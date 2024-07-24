e4da1c04033c06f08e32f7f0aa9d0ea18caab167
Merlinkk <anshulkashyap2504@gmail.com>
2024-07-24 14:54:31 +0530

    test-

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express')
+const app = express()
 
 const middleWare = (req,res,next)=>{
-    const currentTime =  getFormattedTimestamp();
-    console.log(currentTime)
+    const now = new Date();
+    const options = { hour: "2-digit", minute: "2-digit" };
+    const currentTime = now.toLocaleTimeString([], options);
+    const url = req.url
+    console.log(currentTime, url)
+    next();
 }
+
+app.get('/',middleWare, (req,res)=>{
+    console.log('works')
+})
+
+app.listen(5000, ()=>{
+    console.log('listening...')
+})
+
